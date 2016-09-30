c825cb1386c9c976b0ca219fa249a16c66c13805
Practica1 <Practica1@192.168.183.83>
2016-09-30 16:53:49 -0600

    modal de si o no falta agregarla a admin empresas

--- angular/service/modalService.js
+++ angular/service/modalService.js
@@ -55,7 +55,6 @@ angular.module("index")
                     vm.ok = function () {
                         $uibModalInstance.close('si');
                     };
-
                     vm.cancel = function () {
                         $uibModalInstance.dismiss('cancel');
                     };
